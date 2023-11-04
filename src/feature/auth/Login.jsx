2bf9c768abe8eeff592dbb06a87e917ea2ea2c90
Umesh <bartaula.umeshkumar@gmail.com>
2023-11-04 09:23:05 -0500

    login form for mobile screen adjusted

--- src/feature/auth/Login.jsx
+++ src/feature/auth/Login.jsx
@@ -131,7 +131,7 @@ const Login = () => {
     <>
       <div className="h-[3rem]"></div>
       {!isSuccessAddNewUser ? (
-        <div className="m-auto w-[100vw] max-w-lg bg-gray-50 text-black shadow-md shadow-black">
+        <div className="xxsm:m-auto xxsm:w-[100vw] xxsm:max-w-lg min-w-full min-h-screen bg-gray-50 text-black shadow-md shadow-black">
           <div className="bg-sky-400 text-white h-[3rem] flex flex-row items-center justify-evenly font-semibold  ">
             <p
               onClick={() => setAccountOption("login")}
